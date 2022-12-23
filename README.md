aec75b54ee535d489e3df1af0199d0288c48c50d
asd08573064 <xdmanwww@gmail.com>
2022-12-23 12:16:25 +0800

    push all

---
+++ README.md
@@ -0,0 +1,3 @@
+Pytorch implementation of the Soft Nearest Neighbor Loss from the paper "Analyzing and Improving Representations with the Soft Nearest Neighbor Loss".
+
+This implementation is based on the official tensorflow [implementation](https://github.com/abhisharsinha/similarity/blob/master/tensorflow_similarity/losses/softnn_loss.py).
---
+++ SoftNearestNeighborLoss.py
@@ -0,0 +1,44 @@
+import torch
+
+class SoftNearestNeighborLoss(nn.Module):
+    def __init__(self,
+               temperature=100.,
+               cos_distance=True):
+        super(SoftNearestNeighborLoss, self).__init__()
+        
+        self.temperature = temperature
+        self.cos_distance = cos_distance
+
+    def pairwise_cos_distance(self, A, B):
+        query_embeddings = torch.nn.functional.normalize(A, dim=1)
+        key_embeddings = torch.nn.functional.normalize(B, dim=1)
+        distances = 1 - torch.matmul(query_embeddings, key_embeddings.T)
+        return distances
+
+    def forward(self, embeddings, labels):
+        batch_size = embeddings.shape[0]
+        eps = 1e-9
+        
+        pairwise_dist = self.pairwise_cos_distance(embeddings, embeddings)
+        pairwise_dist = pairwise_dist / self.temperature
+        negexpd = torch.exp(-pairwise_dist)
+
+        # creating mask to sample same class neighboorhood
+        pairs_y = torch.broadcast_to(labels, (batch_size, batch_size))
+        mask = pairs_y == torch.transpose(pairs_y, 0, 1)
+        mask = mask.float()
+
+        # creating mask to exclude diagonal elements
+        ones = torch.ones([batch_size, batch_size], dtype=torch.float32).cuda()
+        dmask = ones - torch.eye(batch_size, dtype=torch.float32).cuda()
+
+        # all class neighborhood
+        alcn = torch.sum(torch.multiply(negexpd, dmask), dim=1)
+        # same class neighborhood
+        sacn = torch.sum(torch.multiply(negexpd, mask), dim=1)
+
+        # adding eps for numerical stability
+        # in case of a class having a single occurance in batch
+        # the quantity inside log would have been 0
+        loss = -torch.log((sacn+eps)/alcn).mean()
+        return loss
